93782807d23fa23a78194ce51da9a0c12d50c932
Jeremias Souto <jeresouto4@gmail.com>
2025-04-08 09:53:26 -0300

    Comentamos las funciones del smart contract

--- Software/Votacion.sol
+++ Software/Votacion.sol
@@ -12,22 +12,36 @@ contract Votacion {
         numVotosAzul = 0;
     }
 
+    /*
+     * @dev Esta funcion simplemente vota al partido rojo (+1 voto)
+     */
     function votarRojo() public {
         require(buscarVotante(msg.sender) == false);
         agregarVotante(msg.sender);
         numVotosRojo++;
     }
 
+    /*
+     * @dev Esta funcion simplemente vota al partido azul (+1 voto)
+     */
     function votarAzul() public {
         require(buscarVotante(msg.sender) == false);
         agregarVotante(msg.sender);
         numVotosAzul++;
     }
 
+    /*
+     * @dev Agrega un votante a la lista de votantes
+     * @param Direccion de wallet del votante
+     */
     function agregarVotante(address votante) private {
         listaVotantes.push(votante);
     }
 
+    /*
+     * @dev Verifica si el votante esta en la lista de votantes
+     * @param Direccion de wallet del votante
+     */
     function buscarVotante(address votante) private view returns(bool) {
         for (uint i = 0; i < listaVotantes.length; i++){
             if (listaVotantes[i] == votante){
